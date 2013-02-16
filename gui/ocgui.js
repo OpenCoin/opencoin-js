49c3e0499f48768dbcaf8278d90313ce82ef2027
Joerg Baach <mail@baach.de>
2013-02-16 12:31:03 +0100

    wording

--- gui/ocgui.js
+++ gui/ocgui.js
@@ -200,7 +200,8 @@ $(function(e,data) {
         var auth_info = $('#send input[name="subject"]').val();
         m = wallet.requestSendCoins(amount,auth_info);
         storeDB();
-        var encoded = wallet.armor(m.toJson(),'Some '+wallet.currencyName());
+        var description = 'Some '+wallet.currencyName()+' for you';
+        var encoded = wallet.armor(m.toJson(),description);
         $('#sendmessage').html(encoded);
         $('#sendresult a.email').attr('href','mailto:?subject=Some '+wallet.currencyName()+' for you&body='+escape(encoded));
     });
